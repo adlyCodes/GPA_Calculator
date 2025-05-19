354de5f7f572327fda707f25c83025ba51aa0303
Ahmed Adly <113637234+adlyCodes@users.noreply.github.com>
2025-05-19 22:19:18 +0300

    Update GPA Calculator source code.c

--- GPA Calculator source code.c
+++ GPA Calculator source code.c
@@ -2,21 +2,21 @@
 
 // Function to calculate GPA
 float calculate_gpa(int a, int x[], int y[], float z[]) {
-    float k = 0.0, j = 0.0;
+    float k = 0.0f, j = 0.0f;
     for (int i = 0; i < a; i++) {
         // Determine grade points based on the score
-        if (x[i] >= 0 && x[i] <= 49) { z[i] = 0.0; }
-        else if (x[i] <= 52) { z[i] = 3.0; }
-        else if (x[i] <= 55) { z[i] = 4.0; }
-        else if (x[i] <= 59) { z[i] = 5.0; }
-        else if (x[i] <= 64) { z[i] = 6.0; }
-        else if (x[i] <= 69) { z[i] = 7.0; }
-        else if (x[i] <= 74) { z[i] = 8.0; }
-        else if (x[i] <= 79) { z[i] = 9.0; }
-        else if (x[i] <= 84) { z[i] = 10.0; }
-        else if (x[i] <= 89) { z[i] = 11.0; }
-        else if (x[i] <= 94) { z[i] = 11.5; }
-        else { z[i] = 12.0; }
+        if (x[i] <= 49) { z[i] = 0.0f; }
+        else if (x[i] <= 52) { z[i] = 3.0f; }
+        else if (x[i] <= 55) { z[i] = 4.0f; }
+        else if (x[i] <= 59) { z[i] = 5.0f; }
+        else if (x[i] <= 64) { z[i] = 6.0f; }
+        else if (x[i] <= 69) { z[i] = 7.0f; }
+        else if (x[i] <= 74) { z[i] = 8.0f; }
+        else if (x[i] <= 79) { z[i] = 9.0f; }
+        else if (x[i] <= 84) { z[i] = 10.0f; }
+        else if (x[i] <= 89) { z[i] = 11.0f; }
+        else if (x[i] <= 94) { z[i] = 11.5f; }
+        else { z[i] = 12.0f; }
 
         // Calculate weighted grade points and credit hours
         k += y[i] * z[i];
@@ -28,20 +28,20 @@ float calculate_gpa(int a, int x[], int y[], float z[]) {
 
 // Function to show the result
 void show_result(float gpa, float j) {
-    printf("\nYour GPA: %.2f", gpa);
+    printf("\nGPA: %.2f", gpa);
 
     if (gpa > 0.0 && gpa <= 2.0) {
-        printf(", Your letter Grade: F (Fail)");
+        printf(", Grade: F (Fail)");
     } else if (gpa <= 2.6) {
-        printf(", Your letter Grade: D (Passed)");
+        printf(", Grade: D (Pass)");
     } else if (gpa <= 3.0) {
-        printf(", Your letter Grade: C (Good)");
+        printf(", Grade: C (Good)");
     } else if (gpa <= 3.6) {
-        printf(", Your letter Grade: B (Well-done)");
+        printf(", Grade: B (Very Good)");
     } else if (gpa <= 4.0) {
-        printf(", Your letter Grade: A (Excellent)");
+        printf(", Grade: A (Excellent)");
     }
-    printf("\nTotal achieved Credit Hours: %.2f\n", j);
+    printf("\nCredit Hours: %.2f\n", j);
 }
 
 // Main function
@@ -85,7 +85,7 @@ int main() {
     float gpa = calculate_gpa(a, x, y, z);
 
     // Calculate the total credit hours
-    float total_credits = 0.0;
+    float total_credits = 0.0f;
     for (int i = 0; i < a; i++) {
         total_credits += y[i];
     }
